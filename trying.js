b70082a70b4faf20919d8116f91740ef3b045e60
iammmei <safae.khess@gmail.com>
2023-06-26 11:37:11 +0200

    ...

---
+++ trying.js
@@ -0,0 +1,17 @@
+//fetch API works you pass the url as the first proprety then it has a second proprety
+
+// console;
+// fetch("https://reqres.in/api/users")
+//   //for the fetch to return us a response use dot then
+//   .then((res) => console.log(res));
+
+fetch('https://reqres.in/api/users')
+.then(res => {
+   if(res.ok) {
+    console.log('SUCCES')
+   } else {
+    console.log('Not Successful')
+   }
+})
+.then(data => console.log(data))
+.catch(error => console.log('ERROR'))
